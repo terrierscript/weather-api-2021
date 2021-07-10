a7440241a15939caea4ecea160494838dffea6c3
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-07-10 12:11:10 +0900

    remove comment

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -3,8 +3,6 @@ import { execute } from "../../lib/api"
 
 const handler: NextApiHandler = async (req, res) => {
   const { lat, lon } = req.query
-  // const [lat, lon] = process.env.DARK_SKY_LATLNG.split(",")
-  // console.log(lat, lon)
   if (!lat || !lon) {
     res.status(400).json({ message: "invalid parameter" })
     return
